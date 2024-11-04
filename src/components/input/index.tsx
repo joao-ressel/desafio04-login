f5657e560cfdf1cacf347077f291005213b1d894
joao-ressel <joao.resselti@gmail.com>
2024-11-03 20:41:37 -0500

    feat: install react-hook-form and controller implamentation

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,31 @@
+import React from "react";
 import { InputField, InputWrapper } from "./style";
+import { Control, Controller } from "react-hook-form";
 
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>;
 }
 
-export const Input = ({ placeholder, ...props }: InputProps) => {
+export const Input = ({ placeholder, name, control, ...props }: InputProps) => {
   return (
     <InputWrapper>
-      <InputField placeholder={placeholder} {...props} />
+      <Controller
+        render={({ field: { onChange, onBlur, value, ref } }) => (
+          <InputField
+            placeholder={placeholder}
+            {...props}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+            ref={ref}
+          />
+        )}
+        control={control}
+        name={name}
+      ></Controller>
     </InputWrapper>
   );
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,24 @@
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import { Input } from "../../components/input";
 import { Container, LoginContainer } from "./style";
 
 export const Login = () => {
+  const {
+    watch,
+    control,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const form = watch();
+
+  console.log(form);
   return (
     <Container>
       <LoginContainer>
         <h1>Login</h1>
-        <Input placeholder="Usuario" />
-        <Input placeholder="Senha"/>
+        <Input name="email" placeholder="Email" control={control} />
+        <Input name="password" placeholder="Senha" control={control} />
         <Button>Entrar</Button>
       </LoginContainer>
     </Container>
